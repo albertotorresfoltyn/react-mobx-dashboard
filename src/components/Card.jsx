932ab33286c01fb109a63e38be5cf5fe0da4bca3
alberto <alberto.torres@snappler.com>
2020-04-17 11:48:29 -0300

    grid working... 3 and columns

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,76 @@ import Count from "./Count";
 import Buttons from "./Buttons";
 import imageStore from '../store/ImageStore'
 import { useObserver } from "mobx-react";
+import _ from "lodash";
+import RGL, { WidthProvider } from "react-grid-layout";
+
+const ReactGridLayout = WidthProvider(RGL);
+
+ class BasicLayout extends React.PureComponent {
+  static defaultProps = {
+    className: "layout",
+    items: 20,
+    rowHeight: 30,
+    onLayoutChange: function() {},
+    cols: 3
+  };
+
+  constructor(props) {
+    super(props);
+
+    const layout = this.generateLayout();
+    this.state = { layout };
+  }
+
+  generateDOM() {
+    return _.map(_.range(this.props.items), function(i) {
+      return (
+        <div key={i}>
+          <span className="text">{i}</span>
+        </div>
+      );
+    });
+  }
+
+  generateLayout() {
+    const p = this.props;
+    return _.map(new Array(p.items), function(item, i) {
+      const y = _.result(p, "y") || Math.ceil(Math.random() * 4) + 1;
+      return {
+        x: (i * 2) % 12,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        i: i.toString()
+      };
+    });
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+  }
+
+  render() {
+    return (
+      <ReactGridLayout
+        layout={this.state.layout}
+        onLayoutChange={this.onLayoutChange}
+        {...this.props}
+      >
+        {this.generateDOM()}
+      </ReactGridLayout>
+    );
+  }
+}
+/*
+if (process.env.STATIC_EXAMPLES === true) {
+  import("../test-hook.jsx").then(fn => fn.default(BasicLayout));
+}*/
 
 export default function Card() {
     return (
-        useObserver(() => (
-            <div className="card">
-                <img src={imageStore.imageUrl} className="card-img-top" alt="..." />
-                <button className="btn btn-light" onClick={() => { imageStore.fetchImage() }}>
-                    <i className="fa fa-chevron-right" />
-                </button>
-                <Count />
-                <div className="card-body" >
-                    <Buttons />
-                </div>
-            </div>
+        useObserver(() => (<BasicLayout></BasicLayout>
+            
         ))
     );
 }
